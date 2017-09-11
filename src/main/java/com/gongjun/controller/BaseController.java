a1f60bad13a867bc55c4a709b73dbd891b786e6b
gongj <gongj@chinawiserv,com>
2017-09-11 10:35:21 +0800

    更新文件

---
+++ src/main/java/com/gongjun/controller/BaseController.java
@@ -0,0 +1,15 @@
+package com.gongjun.controller;
+
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * Created with IntelliJ IDEA
+ * Description:
+ * Author:GongJun
+ * Date:2017/8/1
+ * Time:14:27
+ * Chinawiserv Technologies Co., Ltd.
+ */
+@PropertySource("classpath:my.properties")
+public class BaseController {
+}
